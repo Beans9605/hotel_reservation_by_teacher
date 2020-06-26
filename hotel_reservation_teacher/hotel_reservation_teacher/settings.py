d8c2e9b541544b6c760307ff3b155458c37cdc91
Bean9605 <alkad1234@likelion.org>
2020-06-26 17:57:36 +0900

    models update

--- hotel_reservation_teacher/hotel_reservation_teacher/settings.py
+++ hotel_reservation_teacher/hotel_reservation_teacher/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite.apps.MainsiteConfig',
+    'reservation.apps.ReservationConfig',
+    'user_log.apps.User_logConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static')
+)
--- hotel_reservation_teacher/reservation/models.py
+++ hotel_reservation_teacher/reservation/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from user_log.models import User
 
 # Create your models here.
+
+class Room(models.Model) :
+    name = models.CharField(max_length=20)
+    how_many_accept = models.IntegerField(default=2)
+    discription = models.TextField()
+    price = models.IntegerField(default=50000)
+
+
+class Reservation(models.Model) :
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    
+    reservation_start_day = models.DateField(auto_now=True)
+    reservation_end_day = models.DateField(auto_now=True)
+
+    reservation_request_day = models.DateField(auto_now_add=True)
+
+    night_num = models.IntegerField(default=1)
+
+    users_num = models.IntegerField(defautl=2)
