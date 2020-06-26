901423c85fef0d7fc089d5554932d89706d67f10
Bean9605 <alkad1234@likelion.org>
2020-06-26 17:32:45 +0900

    new hotel_reservation by teacher

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alkad\desktop\프로젝트모음\example_hotel_reservation\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
