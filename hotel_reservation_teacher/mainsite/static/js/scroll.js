a3cee9c15e84aa2d79af7b72806ee75473bd5d50
mmajje <bluekmky@likelion.org>
2020-06-28 01:47:40 +0900

    scroll add

---
+++ hotel_reservation_teacher/mainsite/static/js/scroll.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll',function(){
+    const img = document.querySelector('.imgmove')
+    console.log(img)
+    let nowscroll = document.children[0].scrollTop
+    nowscroll = Math.floor(nowscroll)
+    console.log(nowscroll)
+    img.style.transform = `translateY(-${nowscroll/4}px)`
+    navbar(nowscroll)
+    function navbar(scroll){
+        const nav = document.querySelector('.navbar')
+        if (scroll>400){
+            nav.style.width = '100%'
+            nav.style.position = "fixed"
+        }else{
+            nav.style.width = 'none'
+            nav.style.position = "relative"
+        }
+    }
+})
+
--- hotel_reservation_teacher/mainsite/views.py
+++ hotel_reservation_teacher/mainsite/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "mainsite/home.html")
+
+def easy_booking(request):
+    return render(request, "mainsite/home.html")
+
+
+def about(request):
+    return render(request, "about/about.html")
